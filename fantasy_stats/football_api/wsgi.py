5acd6b6abc047327f692a2da7e321858ea86cc51
Minh Tran <tranhuuminh3124@gmail.com>
2022-03-31 19:49:19 +0700

    Rename file from main to wsgi

--- fantasy_stats/football_api/wsgi.py
+++ fantasy_stats/football_api/wsgi.py
@@ -4,11 +4,8 @@ from os import path
 
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-from football_api.constants import DATABASE_LOCATION
-
-from app import create_app
+from app import app
 
 
 if __name__ == "__main__":
-    app = create_app(DATABASE_LOCATION)
     app.run(debug=True)
